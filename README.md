2fa845337e7ae73c6c4450a12a5468c7d714292f
Florent GALLOU <florentgallou@gmail.com>
2021-02-25 23:36:59 +0100

    add var promt userChoice updated README

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ So please, don't push youself and enjoy a pleasant random-chance-base-non-neuron
 > 1. 25-02-2021 : 23h17 - Copied repository to my workstation, creating a README.md, initialized README.md.
 > 2. 25-02-2021 : 23h21 - Added Welcome message.
 > 3. 25-02-2021 : 23h27 - Added user name prompt.
+> 4. 25-02-2021 : 23h35 - Added user choice promt.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 alert("Welcome to you new favorit break in the day\n it's time for\n             ROCK PAPER SCISSORS\n                                         enjoy");
 var userName = prompt("Enter your best gamer Alias :");
-alert(userName);
+var userChoice = prompt("So " + userName + ",\n Please chose between :\n rock or paper or scissors and write it down here.");
+alert("You choosed : " + userChoice);
